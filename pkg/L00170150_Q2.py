99e512f73772f5e392795ee3d21b5081af20b5bd
L00170150 <L00170150@student.lyit.ie>
2021-12-04 00:25:53 +0000

    updated comments and print statement

--- pkg/L00170150_Q2.py
+++ pkg/L00170150_Q2.py
@@ -1,13 +1,13 @@
 """
 #
-# File     :L00170150_Q2.py
-# created  : 28/11/2021 20:34
-# Author   : Neha Tripathi
-# Version  :v1.0.0
-# Licencing : 2021 Neha Tripathi
-# Description : Find all the headings on Apache 2 web page  using lxml parser
-to find the apache2 word in a webpage and to count it
-Find the title of webpage
+# File              :L00170150_Q2.py
+# created           :28/11/2021 20:34
+# Author            :Neha Tripathi
+# Version           :v1.0.0
+# Licencing         :2021 Neha Tripathi
+# Description       :Find the title and headings on Apache 2 web page using lxml parser
+#                    count the apache2 word on web page.
+#
 #
 """
 from bs4 import BeautifulSoup
@@ -18,17 +18,14 @@ source = requests.get('http://192.168.43.128/').text
 
 soup = BeautifulSoup(source, 'lxml')
 
-tag = soup.find('title')   # This will give the title of the Apache2 Page
-print(tag.text)
+tag = soup.find('title')  # This will give the title of the Apache2 Page
+tags = tag.text
+print('Title of the Apache2 webpage is : {}'.format(tags))
 
-for match in soup.find_all('div',class_='section_header'):   # For loop to find all the headers on the Page
+for match in soup.find_all('div', class_='section_header'):  # For loop to find all the headers on the Page
     header = match.text
     print(header)
 
-
-
-count = len(soup.find_all(text=re.compile("apache2"))) +  len(soup.find_all(text=re.compile("Apache2")))
+# Used regular expression to count the apache2 occurrences
+count = len(soup.find_all(text=re.compile("apache2"))) + len(soup.find_all(text=re.compile("Apache2")))
 print('\nUrl: http://192.168.43.128/ contains {} occurrences of word apache2'.format(count))
-
-
-
